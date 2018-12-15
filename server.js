346d2a70e779eedfb4e290f50c817c904b8b3a69
Emanuel Rabina <emanuelrabina@gmail.com>
2018-12-15 13:58:01 +1300

    Get a demo app started

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('dist'));
+app.get('/', (request, response) => {
+	response.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+	console.log(`Demo app listening on port ${port}...`);
+});
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+	entry: './scripts/demo.js',
+	output: {
+		filename: 'demo.js',
+		path: path.resolve(__dirname, 'dist/scripts')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				include: path.resolve(__dirname, 'scripts'),
+				use: 'babel-loader'
+			}
+		]
+	},
+	devtool: '#source-map'
+};
